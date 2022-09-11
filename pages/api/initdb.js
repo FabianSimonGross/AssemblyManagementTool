d6c521885b8037d1db3a02658758e91d1b0e67e4
Fabian Simon Groß <fag8714@thi.de>
2022-09-11 11:06:23 +0300

    [unstable] Improved initdb.js

--- pages/api/initdb.js
+++ pages/api/initdb.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import executeQuery from '../../lib/db'
 
-async function createTable (sql, res) {
+async function executeSqlQueryWithoutValues (sql, res) {
   await executeQuery({
     query: sql,
     values: null
@@ -13,28 +13,33 @@ async function createTable (sql, res) {
 
 export default async function handler (req, res) {
   try {
-    await createTable(
+    await executeSqlQueryWithoutValues(
       'CREATE TABLE IF NOT EXISTS `voting`.`motions` ( `id` INT NOT NULL AUTO_INCREMENT, `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `yes` INT , `no` INT , `abstention` INT , `active` TINYINT(1), `title` TEXT NOT NULL, PRIMARY KEY (id))',
       res
     )
 
-    await createTable(
+    await executeSqlQueryWithoutValues(
       'CREATE TABLE IF NOT EXISTS `voting`.`speakers` ( `id` INT NOT NULL AUTO_INCREMENT, `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `name` TEXT NOT NULL, `gender` TEXT NOT NULL, PRIMARY KEY (id))',
       res
     )
 
-    await createTable(
+    await executeSqlQueryWithoutValues(
       'CREATE TABLE IF NOT EXISTS `voting`.`agendaitems` ( `id` INT NOT NULL AUTO_INCREMENT , `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `title` TEXT NOT NULL, `active` TINYINT(1), PRIMARY KEY (id))',
       res
     )
 
-    await createTable(
+    await executeSqlQueryWithoutValues(
       'CREATE TABLE IF NOT EXISTS `voting`.`settings` (`setting` TEXT NOT NULL, `bool` TINYINT(1))',
       res
     )
 
-    await createTable(
+    await executeSqlQueryWithoutValues(
       'CREATE TABLE IF NOT EXISTS `voting`.`voters` (`user` VARCHAR(250) NOT NULL, `weight` INT NOT NULL, `voted` INT NOT NULL, `expires` TIMESTAMP, `loggedin` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(user))',
+      res
+    )
+
+    await executeSqlQueryWithoutValues(
+      "INSERT INTO `voting`.`settings`(`setting`, `bool`) VALUES ('quotation', 0)",
       res
     )
   } catch (error) {
